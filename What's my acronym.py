5e447eede970e613fe5449b64654f699005c753f
KryhaX <krystiwierciak1@interia.pl>
2022-04-16 16:17:28 +0200

    Finish What's my acronym.py

---
+++ What's my acronym.py
@@ -0,0 +1,21 @@
+# Example
+#Ask the user to enter the full meaning of an organization or concept and you'll provide the acronym to the user. For example:
+
+#Input -> As Soon As Possible. Output -> ASAP.
+#Input -> World Health Organization. Output -> WHO.
+#Input -> Absent Without Leave. Output -> AWOL.
+acronym = ""
+
+full_meaning = str(input("Write Full meaning ,and I'm gonna change it to acronym \n(New word should be a big letter and space ) : "))
+for i in full_meaning:
+    num = ''
+    if i.isdigit():
+        num +=i
+        print("Sory we can't make acronym of:\n{} ".format(num))
+    if i.isupper():
+        acronym += i + '.'
+
+print("You're acronimy of :\n{}\nis\n{}".format(full_meaning,(acronym[:-1])))
+
+
+
